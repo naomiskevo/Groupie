a3b799c628b81928018a7e68d78034d2e69081f0
naomiskevo <naomiskevo@gmsail.com>
2019-12-18 17:14:35 -0600

    adds about page

--- main_app/views.py
+++ main_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return render(request, 'about.html')
