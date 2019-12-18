e6c6f815247affe1decbec93db76a243691e0489
naomiskevo <naomiskevo@gmsail.com>
2019-12-18 17:42:38 -0600

    its broken, search for typos

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Artist:
+    def __init__(self, name):
+        self.name = name
+
+artists = [
+    Artist('Justin Timberlake'),
+    Artist('Chance the Rapper'),
+    Artist('Glassing')
+]
+
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def artists_index(request):
+    return render(request, 'artists/index.html', { 'artists': artists })
