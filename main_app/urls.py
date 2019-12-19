77f25a9c9ee41fb06c7159e9f2d170246fcc19d6
naomiskevo <naomiskevo@gmsail.com>
2019-12-19 15:12:33 -0600

    adds signup url path

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('artists/', views.artists_index, name='index'),
+    path('accounts/signup/', views.signup, name='signup'),
     
 ]
