92afb64d235831742d6451467cffb78d822b39b1
naomiskevo <naomiskevo@gmsail.com>
2019-12-19 15:30:39 -0600

    adds signup function to create a form

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Artist
 import requests
 import os
@@ -19,3 +21,17 @@ def about(request):
 def artists_index(request):
     artists = Artist.objects.all()
     return render(request, 'artists/index.html', { 'artists': artists })
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render (request, 'registration/signup.html', context)
