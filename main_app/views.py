35818752c309261fcf720b09cb2e00a0cf4c54a0
Dustin <dustin.anthony.coffman@gmail.com>
2019-12-28 12:29:37 -0600

    AWS functionality started, manual form field generated

--- main_app/views.py
+++ main_app/views.py
@@ -60,6 +60,25 @@ def show(request):
       'artist_form': artist_form
     }) 
 
+def add_photo(request, event_id):
+	# photo-file was the "name" attribute on the <input type="file">
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # need a unique "key" for S3 / needs image file extension too
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    # just in case something goes wrong
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      # build the full url string
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      # we can assign to cat_id or cat (if you have a cat object)
+      photo = Photo(url=url, event_id=event_id)
+      photo.save()
+    except:
+      print('An error occurred uploading file to S3')
+  return redirect('events/index.html', event_id=event_id)
+  
 
 def home(request):
   return render(request, 'index.html') 
